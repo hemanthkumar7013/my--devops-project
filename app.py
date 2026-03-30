d3426ab9e423baf2389d642efec20458abf0826e
hemanthkumar7013 <hemanthjlk@gmail.com>
2026-03-30 20:52:14 +0530

    updated

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from flask import Flask
+import mysql.connector
+import os
 
 app = Flask(__name__)
 
+db = mysql.connector.connect(
+    host=os.environ['DB_HOST'],
+    user=os.environ['DB_USER'],
+    password=os.environ['DB_PASSWORD'],
+    database=os.environ['DB_NAME']
+)
+
 @app.route('/')
 def home():
-    return "Hello DevOps! 🚀 Your app is running in Kubernetes"
+    return "Flask connected to RDS 🚀"
+
+@app.route('/add')
+def add_user():
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))")
+    cursor.execute("INSERT INTO users (name) VALUES ('Hemanth')")
+    db.commit()
+    return "User added to RDS ✅"
+
+@app.route('/users')
+def users():
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+    result = cursor.fetchall()
+    return str(result)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
--- dockerfile
+++ dockerfile
@@ -1,7 +1,16 @@
-FROM python:3.8-slim
+# Stage 1: Builder
+FROM python:3.11-slim as builder
 WORKDIR /app
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-COPY . .    
-EXPOSE 5000
-CMD ["python", "app.py"]
+RUN pip install --user --no-cache-dir -r requirements.txt
+# Stage 2: Runtime
+FROM python:3.11-slim
+WORKDIR /app
+# Copy Python dependencies from builder
+COPY --from=builder /root/.local /root/.local
+# Copy application code
+COPY . .
+# Set PATH to use local pip packag
+ENV PATH=/root/.local/bin:$PATH
+# set the entry point to run the application
+ENTRYPOINT ["python"]
